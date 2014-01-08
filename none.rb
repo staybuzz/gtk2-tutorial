cd037904fe9e41a7f3ec3b12d26ebc6f4676c7fd
staybuzz <staybuzz.k@gmail.com>
2014-01-08 18:19:24 +0900

    ラベルを表示

--- none.rb
+++ none.rb
@@ -9,5 +9,10 @@ win.signal_connect("destroy") do
   Gtk.main_quit()         #GTKを終了
 end
 
+lbl = Gtk::Label.new()    #ラベルのウィジェットを生成
+lbl.set_text("label")     #文字列"label"のセット
+win.add(lbl)              #ウィンドウにラベルをパッキング
+
 win.show()                #ウィンドウを表示状態に変更
+lbl.show()                #ラベルを表示
 Gtk.main()                #GTKを実行
