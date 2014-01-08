329236009bc8fa9e88c67fee57af416a2a41414a
staybuzz <staybuzz.k@gmail.com>
2014-01-08 18:15:32 +0900

    終了時のシグナルハンドラを追加

--- none.rb
+++ none.rb
@@ -4,5 +4,10 @@
 require 'gtk2'
 
 win = Gtk::Window.new()   #ウィンドウのウィジェットを作成
+
+win.signal_connect("destroy") do
+  Gtk.main_quit()         #GTKを終了
+end
+
 win.show()                #ウィンドウを表示状態に変更
 Gtk.main()                #GTKを実行
