9a15812069dc74a4df20b529e6f38eb63fdb91bd
staybuzz <staybuzz.k@gmail.com>
2014-01-08 18:22:29 +0900

    ラベルを表示,コンパクトに

--- none.rb
+++ none.rb
@@ -9,10 +9,15 @@ win.signal_connect("destroy") do
   Gtk.main_quit()         #GTKを終了
 end
 
-lbl = Gtk::Label.new()    #ラベルのウィジェットを生成
-lbl.set_text("label")     #文字列"label"のセット
+lbl = Gtk::Label.new("label")    #ラベルのウィジェットを生成
+
+#下の2行は上の1行でまとめられる
+#
+#lbl = Gtk::Label.new()
+#lbl.set_text("label")     #文字列"label"のセット
 win.add(lbl)              #ウィンドウにラベルをパッキング
 
-win.show()                #ウィンドウを表示状態に変更
-lbl.show()                #ラベルを表示
+win.show_all()    #下2行をまとめて書く
+#win.show()                #ウィンドウを表示状態に変更
+#lbl.show()                #ラベルを表示
 Gtk.main()                #GTKを実行
